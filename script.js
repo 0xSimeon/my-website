4adc3c48381e6a24be42888200c80a3d5a8f7542
simeon4real <simeon.udoh45@gmail.com>
2020-05-20 13:07:35 +0100

    emperimental feature

--- script.js
+++ script.js
@@ -1,2 +1,23 @@
-const date = new Date().getFullYear();
-document.getElementById('year').textContent = date; 
+const hamburger = document.querySelector('.hamburger');
+const navList = document.querySelector('.nav__list'); 
+const links = document.querySelectorAll('.nav__item');
+const body = document.querySelector('body');
+
+
+
+hamburger.addEventListener('click', () => {
+    body.classList.toggle('scroll');
+    navList.classList.toggle('open'); 
+    links.forEach(link => {
+        link.classList.toggle('fade'); 
+    }) 
+
+});  
+
+
+
+
+
+// const date = new Date().getFullYear();
+// document.getElementById('year').textContent = date; 
+
