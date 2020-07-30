4612fe12ec9b3ef638503e1028cc4b168f7b7d50
simeon4real <simeon.udoh45@gmail.com>
2020-07-30 21:36:17 +0100

    Added Project component :zap:

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,30 @@
 <template>
-  <div id="app" class="">
+  <div id="app">
     <app-nav></app-nav>
     <app-header></app-header>
     <app-hero></app-hero>
     <app-services></app-services>
+    <app-projects></app-projects>
   </div>
 </template>
 
 <script>
-import Header from './components/Header';
-import Hero from './components/Hero.vue';
-import Nav from './components/Nav.vue';
-import Services from './components/Services.vue';
+import Header from '@/components/Header';
+import Hero from '@/components/Hero.vue';
+import Nav from '@/components/Nav.vue';
+import Services from '@/components/Services.vue';
+import Projects from '@/components/Projects.vue';
 export default {
   name: 'App',
   components: {
     appHeader: Header,
     appHero: Hero,
     appNav: Nav,
-    appServices: Services
+    appServices: Services,
+    appProjects: Projects
   }
 };
 </script>
 
 <style lang="scss">
-#app {
-  background: rgba($color-light, 0.5);
-}
 </style>
